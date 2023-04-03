d2bf0d9cc05f7b4e475ad1bd143807983f9d657e
ddl13 <ddl@lytvyn.com>
2023-04-03 21:05:39 +0300

    markup(downloaded) and tabs (own js))

---
+++ js/script.js
@@ -0,0 +1,38 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelectorAll(".tabheader__item");
+  const tabsContent = document.querySelectorAll(".tabcontent");
+  const tabsParent = document.querySelector(".tabheader__items");
+
+  function hideTabFunction() {
+    tabsContent.forEach((el) => {
+      el.classList.add("hide");
+      el.classList.remove("show", "fade");
+    });
+
+    tabs.forEach((tab) => {
+      tab.classList.remove("tabheader__item_active");
+    });
+  }
+
+  function showTabFunction(i = 0) {
+    tabsContent[i].classList.add("show", "fade");
+    tabsContent[i].classList.remove("hide");
+    tabs[i].classList.add("tabheader__item_active");
+  }
+
+  hideTabFunction();
+  showTabFunction();
+
+  tabsParent.addEventListener("click", (event) => {
+    const target = event.target;
+
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((el, i) => {
+        if (target === el) {
+          hideTabFunction();
+          showTabFunction(i);
+        }
+      });
+    }
+  });
+});
