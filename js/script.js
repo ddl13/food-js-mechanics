04940fc745ef1e0c97ab0632f1f85b8838280d4f
ddl13 <ddl@lytvyn.com>
2023-04-21 01:47:55 +0300

    simple slider

--- js/script.js
+++ js/script.js
@@ -138,5 +138,103 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  const modalTimerId = setTimeout(openModal, 10000);
+  // const modalTimerId = setTimeout(openModal, 10000);
+
+  // Use classes for cards
+
+  // class MenuCards {
+  //   constructor(src, alt, title, description, price, parentSelector) {
+  //     this.src = src;
+  //     this.alt = alt;
+  //     this.title = title;
+  //     this.description = description;
+  //     this.price = price;
+  //     this.transfer = 30;
+  //     this.parent = document.querySelector(parentSelector);
+  //     this.changeToHryvna();
+  //   }
+
+  //   changeToHryvna() {
+  //     this.price = this.price * this.transfer;
+  //   }
+
+  //   render() {
+  //     const element = document.createElement("div");
+  //     element.innerHTML = `<div class="menu__item">
+  //           <img src=${this.src} alt=${this.alt} />
+  //           <h3 class="menu__item-subtitle">${this.title}</h3>
+  //           <div class="menu__item-descr">
+  //             ${this.description}
+  //           </div>
+  //           <div class="menu__item-divider"></div>
+  //           <div class="menu__item-price">
+  //             <div class="menu__item-cost">Цена:</div>
+  //             <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+  //           </div>
+  //         </div>`;
+
+  //     this.parent.append(element);
+  //   }
+  // }
+
+  // new MenuCards(
+  //   "img/tabs/vegy.jpg",
+  //   "vegy",
+  //   'Меню "Фитнес"',
+  //   'Меню "Фитнес" - это новый подход к приготовлению блюд: больше
+  //             свежих овощей и фруктов. Продукт активных и здоровых людей. Это
+  //             абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+  //   9,
+  //   ".menu .container"
+  // ).render();
+
+  // slider
+
+  const slides = document.querySelectorAll(".offer__slide"),
+    prev = document.querySelector(".offer__slider-prev"),
+    next = document.querySelector(".offer__slider-next"),
+    total = document.querySelector("#total"),
+    current = document.querySelector("#current");
+
+  let slideIndex = 1;
+
+  showSlides(slideIndex);
+
+  if (slides.length < 10) {
+    total.textContent = `0${slides.length}`;
+  } else {
+    total.textContent = `${slides.length}`;
+  }
+
+  function showSlides(n) {
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+
+    slides.forEach((item) => (item.style.display = "none"));
+
+    slides[slideIndex - 1].style.display = "block";
+
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = `${slideIndex}`;
+    }
+  }
+
+  function plusSlides(n) {
+    showSlides((slideIndex += n));
+  }
+
+  prev.addEventListener("click", () => {
+    plusSlides(-1);
+  });
+
+  next.addEventListener("click", () => {
+    plusSlides(1);
+  });
 });
